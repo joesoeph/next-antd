4cd6ca8e1264ac1b20c2eae718b4a7daccbff128
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-06-03 11:15:55 +0700

    create gendateMock to generate posts data

---
+++ generateMock.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const { faker } = require('@faker-js/faker');
+
+const generatePosts = (count) => {
+    const data = {posts: []};
+
+  for (let i = 0; i < count; i++) {
+    const post = {
+      id: i + 1,
+      title: faker.lorem.words(),
+      content: faker.lorem.paragraph(),
+      image: faker.image.urlLoremFlickr(),
+    };
+
+    data.posts.push(post);
+  }
+
+  return data;
+};
+
+const saveDataToFile = (data) => {
+  const jsonData = JSON.stringify(data, null, 2);
+  fs.writeFileSync('mockdb.json', jsonData);
+};
+
+const posts = generatePosts(1000);
+saveDataToFile(posts);
