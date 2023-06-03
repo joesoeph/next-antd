ae70cfd5075a499e313dd6e9d06e88d34173ebd4
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-06-03 16:19:15 +0700

    implement swr to fetching posts

---
+++ src/app/components/ListPosts.js
@@ -0,0 +1,45 @@
+'use client';
+
+import { useState } from 'react';
+import useSWR from 'swr';
+import Image from 'next/image';
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function ListPosts() {
+  let [counter, useCounter] = useState(1);
+  const { data, error, isLoading } = useSWR(
+    'http://localhost:3030/posts?_page=' + counter,
+    fetcher
+  );
+  if (error) return 'An error has occurred.';
+  if (isLoading) return 'Loading...';
+  return (
+    <>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>
+            <Image src={item.image} alt="Test" width={200} height={200} />
+            {/* <p>{item.image}</p> */}
+            <p>{item.title}</p>
+          </li>
+        ))}
+      </ul>
+      <p>{counter}</p>
+      <button
+        onClick={function () {
+          useCounter(--counter);
+        }}
+      >
+        Previous
+      </button>
+      <button
+        onClick={function () {
+          useCounter(++counter);
+        }}
+      >
+        Next
+      </button>
+    </>
+  );
+}
