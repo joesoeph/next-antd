c6058160bd501d71d6f06877e7064096230c60b8
Yusuf Fazeri <yusuffazeri00@gmail.com>
2023-06-10 09:46:50 +0700

    implement meta html and separate component

--- src/app/content/[slug]/page.js
+++ src/app/content/[slug]/page.js
@@ -1,24 +1,19 @@
-import Image from 'next/image';
+import DetailPost from '@/app/content/components/DetailPost';
 
 async function getDetailContent(id) {
   const response = await fetch('http://localhost:3030/posts/' + id);
   return response.json();
 }
 
+export async function generateMetadata({ params }) {
+  const post = await getDetailContent(params.slug);
+
+  return {
+    title: post.title,
+  };
+}
+
 export default async function ContentDetail({ params }) {
   const post = await getDetailContent(params.slug);
-  console.log(params);
-  return (
-    <>
-      <Image
-        src={post.image}
-        sizes="(max-width: 768px) 100vw"
-        width={500}
-        height={500}
-        alt={post.image}
-      />
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
-    </>
-  );
+  return <DetailPost post={post} />;
 }
